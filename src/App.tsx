3590599da603da608b0cb2d8157cb7d6889bb7d6
chad hinds <chadrickj8@gmail.com>
2023-04-26 11:58:04 -0400

    Remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// Type we need for our state
+interface UserInputType {
+  username: string
+  pw: string
+}
 
+const initialState = { username: "", pw: "" }
+
+function App() {
+  useState<UserInputType>(initialState)
+
+  // Use state to save the value from inputs
+  const [userInput, setUserInput] = useState<UserInputType>(initialState);
+
+  function setUsername(e: ChangeEvent<HTMLInputElement>) {
+    setUserInput({ ...userInput, username: e.target.value });
+  }
+
+  function setPw(e: ChangeEvent<HTMLInputElement>) {
+    setUserInput({ ...userInput, pw: e.target.value });
+  }
+  return (
+    <div>
+      <h3>Username</h3>
+      <input type="text" onChange={(e) => setUsername(e)}></input>
+      <h3>Password</h3>
+      <input type="text" onChange={(e) => setPw(e)}></input>
     </div>
   );
 }
